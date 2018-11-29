65b930ac40f18b62ac77fb2b7b43bf15b6d051e7
sgrace1 <43154995+sgrace1@users.noreply.github.com>
2018-11-28 20:18:21 -0700

    Add files via upload
    Working files

---
+++ Tracking-Tree/Tracking-Tree/main.cpp
@@ -0,0 +1,20 @@
+// Main
+// Tracking-Tree
+// Spencer Grace, Ryan Evans, Owen Radcliffe
+
+#include"main.h"
+#include<iostream>
+#include<vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() 
+{
+
+
+
+	return 0;
+}
