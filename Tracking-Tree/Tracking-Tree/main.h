110ee5c22e72a4898539486b8f4aa993b2f23ea9
revansUW <43155795+revansUW@users.noreply.github.com>
2018-11-28 20:21:35 -0700

    Update main.h

--- Tracking-Tree/Tracking-Tree/main.h
+++ Tracking-Tree/Tracking-Tree/main.h
@@ -1,1 +1,19 @@
+#ifndef _T_TREE_H_
+#define _T_TREE_H_
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class BinaryTree
+{
+TNode();
+~TNode();
+};
+
+#endif
